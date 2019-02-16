6201850a1aa4e4a067ae3a0d8e8fe5e5dcafdf83
Jose Antonio Rosa dos Santos Junior <joses.jr@gmail.com>
2019-02-16 13:29:52 -0200

    Fix Controller endpoints. Added form to html page

--- src/main/java/jj/ubs/controller/UbsController.java
+++ src/main/java/jj/ubs/controller/UbsController.java
@@ -3,31 +3,30 @@ package jj.ubs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jj.ubs.domain.UbsService;
 import jj.ubs.domain.infra.Record;
 import lombok.AllArgsConstructor;
 
 @Controller
-@RequestMapping("/ubs")
 @AllArgsConstructor
 public class UbsController {
 
-    private UbsService ubsService;
+    private final UbsService ubsService;
 
-    @GetMapping
-    public String getNearestUbs(Model model) {//, @RequestParam String city, @RequestParam String lattitude, @RequestParam String longitude) {
-        //        Record ubs = new Record();
-        //        ubs.setCodMunic("421770");
-        //        ubs.setDscCidade("Sombrio");
-        //        ubs.setDscEndereco("RUA ANICETO SILVEIRA");
-        //        ubs.setVlrLatitude("-29.1107439994804");
-        //        ubs.setVlrLongitude("-49.6437835693345");
-        //        model.addAttribute("ubs", ubs);
-        Record record = ubsService.getNearestUbs("São José dos Campos", -23.2365917, 45.9234772);
+    @GetMapping("/")
+    public String index() {
+        return "localizar";
+    }
+
+    @PostMapping("/localizar")
+    public String getNearestUbs(final Model model, @RequestParam final String city,
+            @RequestParam final Double lattitude, @RequestParam final Double longitude) {
+        final Record record = ubsService.getNearestUbs(city, lattitude, longitude);
         model.addAttribute("ubs", record);
-        return "index";
+        return "localizar";
     }
 
 }
