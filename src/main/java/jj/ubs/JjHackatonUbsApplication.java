74e5af945462f590b33a31bc20ade93625381ccf
Ederson Donde <edersondonde@gmail.com>
2019-02-16 09:49:03 -0200

    First version

---
+++ src/main/java/jj/ubs/JjHackatonUbsApplication.java
@@ -0,0 +1,14 @@
+package jj.ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjHackatonUbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjHackatonUbsApplication.class, args);
+	}
+
+}
+
